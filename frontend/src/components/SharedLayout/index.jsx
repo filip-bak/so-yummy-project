f4f42d9c412354a46a2d6b393a9fae63da32d8ba
Izabela Świadkowska <izaswiadkowska@gmail.com>
2023-10-28 11:10:24 +0200

    renamed folder policy added

--- frontend/src/components/SharedLayout/index.jsx
+++ frontend/src/components/SharedLayout/index.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
 import Loader from "components/Loader";
 import styles from "./SharedLayout.module.css";
+import PolicyAndTerms from "components/PolicyAndTerms";
 
 const SharedLayout = () => {
   return (
@@ -11,6 +12,7 @@ const SharedLayout = () => {
         <Outlet />
       </Suspense>
       Footer
+      <PolicyAndTerms />
     </div>
   );
 };
