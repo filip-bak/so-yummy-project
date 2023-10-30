b52ded7c611c9b5b7ce5f42485704e8d8cb29946
Filip Bączkowski <filo.b@wp.pl>
2023-10-30 16:01:23 +0100

    fix: redux-persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore } from "redux-persist";
 import { recipesReducer } from "./recipes/slice";
 
 export const store = configureStore({
   reducer: { recipes: recipesReducer },
 });
-
-export const persistor = store;
