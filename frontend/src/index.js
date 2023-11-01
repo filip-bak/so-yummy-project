d7a28e7e25e59b37ebcdb5bccfbc31032a6151c7
Filip Bączkowski <filo.b@wp.pl>
2023-11-01 02:25:03 +0100

    feat: add redux-persist config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { App } from "./App";
+import "./index.css";
+import Loader from "components/Loader";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/so-yummy-project">
       <Provider store={store}>
-        <App />
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
