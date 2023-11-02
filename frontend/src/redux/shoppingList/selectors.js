38c40b85eceadbb5485e26eae324568ff304a7fc
basiacarvalho <basiacarvalho@gmail.com>
2023-11-02 18:54:36 +0100

    removing unneeded selectors

--- frontend/src/redux/shoppingList/selectors.js
+++ frontend/src/redux/shoppingList/selectors.js
@@ -1,5 +1,3 @@
 export const selectShoppingList = state => {
   return state.shoppingList.items;
 };
-export const selectIsLoading = state => state.shoppingList.isLoading;
-export const selectError = state => state.ShoppingList.error;
