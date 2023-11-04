338b0430c65d0578b4555b4f952e33a93c8c9a1f
Filip Bączkowski <filo.b@wp.pl>
2023-11-04 05:24:12 +0100

    feat: add restricted and private routes

--- frontend/src/LayoutRoutes/RestrictedRoute.jsx
+++ frontend/src/LayoutRoutes/RestrictedRoute.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { Suspense } from "react";
+import PropTypes from "prop-types";
+import { Navigate, useLocation } from "react-router-dom";
+import { selectToken } from "redux/auth/selectors";
+import Loader from "components/Loader";
+import { useSelector } from "react-redux";
 
-const RestrictedRoute = props => {
-  return <div>RestrictedRoute</div>;
+const RestrictedRoute = ({ component: Component, redirect = "/" }) => {
+  const location = useLocation();
+  const isAuthenticated = useSelector(selectToken);
+  const redirectTo = location.state?.from || redirect;
+
+  return isAuthenticated ? (
+    <Navigate to={redirectTo} state={{ from: location }} replace />
+  ) : (
+    <Suspense fallback={<Loader />}>{Component}</Suspense>
+  );
 };
 
-RestrictedRoute.propTypes = {};
+RestrictedRoute.propTypes = {
+  component: PropTypes.node.isRequired,
+  redirect: PropTypes.string,
+};
 
 export default RestrictedRoute;
