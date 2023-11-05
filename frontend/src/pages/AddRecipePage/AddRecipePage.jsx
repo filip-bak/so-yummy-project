dca6d53b89eac17fe01443e06e84053168e34abc
basiacarvalho <basiacarvalho@gmail.com>
2023-11-05 19:58:21 +0100

    added all the components for AddRecipePage and mobile layout

--- frontend/src/pages/AddRecipePage/AddRecipePage.jsx
+++ frontend/src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,13 +1,33 @@
 import css from "./AddRecipePage.module.css";
 import { MainPageTitle } from "components/MainPageTitle/MainPageTitle";
 import { AddRecipeForm } from "./components/AddRecipeForm/AddRecipeForm";
+import { FollowUs } from "./components/FollowUs/FollowUs";
+import { PopularRecipe } from "./components/PopularRecipe/PopularRecipe";
+import { useEffect, useState } from "react";
 
 const AddRecipesPage = () => {
+  const [showFollowUs, setShowFollowUs] = useState(false);
+
+  useEffect(() => {
+    const updateDimension = () => {
+      setShowFollowUs(window.innerWidth >= 1440);
+    };
+    window.addEventListener("resize", updateDimension);
+
+    updateDimension();
+
+    return () => {
+      window.removeEventListener("resize", updateDimension);
+    };
+  }, [window.innerWidth]);
+
   return (
     <section className={css.section}>
       <div className={css.container}>
         <MainPageTitle title="Add recipe" />
         <AddRecipeForm />
+        {showFollowUs && <FollowUs />}
+        <PopularRecipe />
       </div>
     </section>
   );
