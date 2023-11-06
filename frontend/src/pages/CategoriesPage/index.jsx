1f0d1f3ed219dcfd396f08e9fef3a748668f9a90
Izabela Świadkowska <izaswiadkowska@gmail.com>
2023-11-06 15:47:05 +0100

    onclick to cardmeal added

--- frontend/src/pages/CategoriesPage/index.jsx
+++ frontend/src/pages/CategoriesPage/index.jsx
@@ -27,21 +27,3 @@ const CategoriesPage = () => {
 };
 
 export default CategoriesPage;
-import css from "./CategoriesPage.module.css";
-import { MainPageTitle } from "components/MainPageTitle/MainPageTitle";
-import { CategoryRecipes } from "./components/CategoryRecipes/CategoryRecipes";
-/*import { CategoryList } from "components/CategoriesPage/CategoryList/CategoryList";*/
-
-const CategoriesPage = () => {
-  return (
-    <section className={css.section}>
-      <div className={css.container}>
-        <MainPageTitle title="Categories" />
-
-        <CategoryRecipes />
-      </div>
-    </section>
-  );
-};
-
-export default CategoriesPage;
