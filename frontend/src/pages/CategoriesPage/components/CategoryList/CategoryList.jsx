9bc0edcb8968074a4e091b577276603672d68dc4
Filip Bączkowski <filo.b@wp.pl>
2023-11-06 16:36:14 +0100

    fix: errors

--- frontend/src/pages/CategoriesPage/components/CategoryList/CategoryList.jsx
+++ frontend/src/pages/CategoriesPage/components/CategoryList/CategoryList.jsx
@@ -16,7 +16,6 @@ export const CategoryList = () => {
     dispatch(fetchRecipesCategoryList());
   }, [dispatch]);
 
-
   const handleChange = (event, newValue) => {
     navigate(`/categories/${newValue}`);
   };
