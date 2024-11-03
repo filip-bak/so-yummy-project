b657e00a9bf65b78b51d1ce958f53a38114c7915
Filip Bączkowski <filo.b@wp.pl>
2024-11-03 07:34:17 +0100

    refactor: email verification message to be more clear.

--- backend/modules/users/users-mailer.service.js
+++ backend/modules/users/users-mailer.service.js
@@ -4,9 +4,13 @@ const { sendMail } = require("../mailer/mailer.service");
 const sendUserVerificationMail = async (email, verificationToken) => {
   const mailOptions = {
     to: email,
-    subject: "Welcome to our site!",
-    text: `Hello! Please verify your account by visiting ${config.hostURL}/api/users/verify/${verificationToken}`,
-    html: `<h2>Hello!</h2><br/>Please verify your account by clicking <a href="${config.hostURL}/api/users/verify/${verificationToken}">here</a>!`,
+    subject: "Welcome to So Yummy Project!",
+    text: `Hi Please verify your account by clicking ${config.hostURL}/users/verify/${verificationToken}`,
+    html: `<h2>Hi</h2><br/>Please verify your account by clicking <a href="${config.hostURL}/users/verify/${verificationToken}">
+            <button style="background-color: #6082B6; color: white; padding: 10px 15px; border: none; border-radius: 5px; cursor: pointer;">
+                Verify
+            </button>
+        </a>`,
   };
 
   await sendMail(mailOptions);
